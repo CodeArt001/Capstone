45139972cf0b8e099bdda2d9352a83384b69f156
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2026-09-10 09:03:14 -0400

    env file

--- src/main/java/com/example/demo/CapstoneApplication.java
+++ src/main/java/com/example/demo/CapstoneApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.ignoreIfMissing()
+				.load()
+				.entries()
+				.forEach(entry -> System.setProperty(entry.getKey(),
+						System.getProperty(entry.getKey(), entry.getValue())));
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
 
